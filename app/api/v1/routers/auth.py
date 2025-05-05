f6ae4927b4e4781c5dfd70ba609bf444a863e641
Jumanazarov-Shukrullo <shukrullo.coder@gmail.com>
2025-05-05 18:27:38 +0300

    update

--- app/api/v1/routers/auth.py
+++ app/api/v1/routers/auth.py
@@ -5,6 +5,5 @@ router = APIRouter()
 
 @router.get("/token", summary="Generate a demo JWT (no creds required)")
 def generate_token():
-    # you could embed any payload you like here
     token = create_access_token({"sub": "demo-user"}, expires_delta=60*24)
     return {"access_token": token, "token_type": "bearer"}
